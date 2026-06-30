ccf6f8199135457f69c609ff7ae78902ccbc9b87
Mark Wayne Buncaras Menorca <marcuwynu23@gmail.com>
2026-07-01 00:41:01 +0800

    refactor: move Cloudflare provider configuration from main.tf to providers.tf

--- providers.tf
+++ providers.tf
@@ -5,13 +5,16 @@ terraform {
   backend "gcs" {}
 
   required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 5.19"
+    }
     google = {
       source  = "hashicorp/google"
       version = "~> 7.38"
     }
   }
 }
-
 
 provider "cloudflare" {
   api_token = var.cloudflare_api_token
