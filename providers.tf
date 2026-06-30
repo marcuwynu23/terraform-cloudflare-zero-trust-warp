35d3803ecb0c93089721e5977e54d89070496ac2
Mark Wayne Buncaras Menorca <marcuwynu23@gmail.com>
2026-07-01 00:35:07 +0800

    feat: reorganize Terraform provider configuration in providers.tf

--- providers.tf
+++ providers.tf
@@ -1,4 +1,17 @@
 
+terraform {
+  required_version = ">= 1.0"
+
+  backend "gcs" {}
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 7.38"
+    }
+  }
+}
+
 
 provider "cloudflare" {
   api_token = var.cloudflare_api_token
